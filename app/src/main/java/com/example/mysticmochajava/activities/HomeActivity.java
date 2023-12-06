abbcdfd28206a88b44b7dc72fe442d7661a4b54d
Lara RG <laragoblin@outlook.com>
2023-12-06 17:09:33 +0100

    (◕ᴥ◕ʋ) Profile/Home DONE

--- app/src/main/java/com/example/mysticmochajava/activities/HomeActivity.java
+++ app/src/main/java/com/example/mysticmochajava/activities/HomeActivity.java
@@ -1,47 +1,50 @@
 package com.example.mysticmochajava.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mysticmochajava.R;
+import com.example.mysticmochajava.activities.cafe.CafeMenuActivity;
+import com.example.mysticmochajava.activities.cafe.ShopActivity;
+import com.example.mysticmochajava.activities.library.LibraryActivity;
+import com.example.mysticmochajava.activities.user.ProfileActivity;
 import com.google.android.material.navigation.NavigationView;
 
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private DrawerLayout drawerLayout;
+public class HomeActivity extends AppCompatActivity{
+    Button CafeBtn, ShopBtn, BookBtn, ProfileBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_home);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        drawerLayout = findViewById(R.id.home_drawer);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-
+    }
+    public void OnClickCafe(View v){
+        Intent intent = new Intent(this, CafeMenuActivity.class);
+        startActivity(intent);
+    }
+    public void OnClickBook(View v){
+        Intent intent = new Intent(this, LibraryActivity.class);
+        startActivity(intent);
+    }
+    public void OnClickShop(View v){
+        Intent intent = new Intent(this, ShopActivity.class);
+        startActivity(intent);
+    }
+    public void OnClickProfile(View v){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-        super.onPointerCaptureChanged(hasCapture);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
-    }
 }
