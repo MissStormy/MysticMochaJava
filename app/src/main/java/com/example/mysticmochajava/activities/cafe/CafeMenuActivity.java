361dff7dadc9254458ef73d33c4989c6d738fa85
Lara RG <laragoblin@outlook.com>
2023-12-08 17:54:30 +0100

    Cafe/Shop More or less

--- app/src/main/java/com/example/mysticmochajava/activities/cafe/CafeMenuActivity.java
+++ app/src/main/java/com/example/mysticmochajava/activities/cafe/CafeMenuActivity.java
@@ -3,17 +3,29 @@ package com.example.mysticmochajava.activities.cafe;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mysticmochajava.R;
+import com.example.mysticmochajava.activities.HomeActivity;
 
 public class CafeMenuActivity extends AppCompatActivity {
-
+    Button BtnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_cafe_menu);
+    }
+    public void OnClickBack(View v){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+    public void OnClickShop(View v){
+        Intent intent = new Intent(this, ShopActivity.class);
+        startActivity(intent);
     }
 }
